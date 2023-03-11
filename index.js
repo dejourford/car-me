3fccfbcb368e4ebb1dd06563ed6abf3e8a4dd934
dejourford <dejourford@my.unt.edu>
2023-03-11 14:38:01 -0600

    add id tag to scrollbar

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ showMenu = () => {
 closeMenu = () => {
     mobileNav.classList.remove('active')
 }
+
+// SCROLL BUTTON
+const scrollUpBtn = document.querySelector('#scroll-up-arrow').addEventListener('click', () => {
+    window.scrollTo(0, 0);
+})
+window.addEventListener('scroll', () => {
+    const scrollUpArrow = document.getElementById('scroll-up-arrow');
+  
+    if (window.scrollY > 500) {
+      scrollUpArrow.classList.remove('hidden');
+    } else {
+      scrollUpArrow.classList.add('hidden');
+    }
+  });
