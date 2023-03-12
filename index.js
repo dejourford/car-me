ad288ce7ac0b9ba390bbd233fa5237c7ce00cbc4
dejourford <dejourford@my.unt.edu>
2023-03-12 17:34:59 -0500

    add on click function

--- index.js
+++ index.js
@@ -15,11 +15,25 @@ const scrollUpBtn = document.querySelector('#scroll-up-arrow').addEventListener(
     window.scrollTo(0, 0);
 })
 window.addEventListener('scroll', () => {
-    const scrollUpArrow = document.getElementById('scroll-up-arrow');
-  
-    if (window.scrollY > 500) {
-      scrollUpArrow.classList.remove('hidden');
-    } else {
-      scrollUpArrow.classList.add('hidden');
-    }
-  });
+  const scrollUpArrow = document.getElementById('scroll-up-arrow');
+
+  if (window.scrollY > 500) {
+    scrollUpArrow.classList.remove('hidden');
+  } else {
+    scrollUpArrow.classList.add('hidden');
+  }
+});
+
+
+// FAQ DROPDOWN BOX
+const questionOne = document.querySelector('.faq-1')
+const questionTwo = document.querySelector('.faq-2')
+const questionThree = document.querySelector('.faq-3')
+const answerOne = document.querySelector('#answer-1')
+const answerTwo = document.querySelector('#answer-2')
+const answerThree = document.querySelector('#answer-3')
+
+showAnswer = () => {
+  // answerOne.classList.toggle('answer')
+  answerOne.classList.toggle('active')
+}
