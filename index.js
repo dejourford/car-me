dc892f07020709d987a7f95bc0c2206eaedc23c5
dejourford <dejourford@my.unt.edu>
2023-03-14 22:19:18 -0500

    add page links

--- index.js
+++ index.js
@@ -11,20 +11,26 @@ closeMenu = () => {
     mobileNav.classList.remove('show')
 }
 
-// SCROLL BUTTON
-const scrollUpBtn = document.querySelector('#scroll-up-arrow').addEventListener('click', () => {
-    window.scrollTo(0, 0);
-})
-window.addEventListener('scroll', () => {
-  const scrollUpArrow = document.getElementById('scroll-up-arrow');
+// Scroll back to top
+// Get the button:
+let mybutton = document.getElementById("scroll-up-arrow");
 
-  if (window.scrollY > 500) {
-    scrollUpArrow.classList.remove('hidden');
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
   } else {
-    scrollUpArrow.classList.add('hidden');
+    mybutton.style.display = "none";
   }
-});
+}
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
 // accordion js
