373090aa2a66bfd45c853a28d551b25a3796c1b7
De'Jour Ford <dejourford@my.unt.edu>
2023-03-18 18:46:53 -0500

    create array for dom manipulation

--- index.js
+++ index.js
@@ -55,58 +55,127 @@ const make = document.querySelector('#make')
 const doors = document.querySelector('#doors')
 const transmission = document.querySelector('#transmission')
 const fuel = document.querySelector('#fuel')
+const rate = document.querySelector('#rate')
+const image = document.querySelector('#car')
 
 // array of cars
 const cars = {
-  'Honda Odyssey': {
+  'Honda_Odyssey': {
     'make': 'Honda',
     'model': 'Odyssey',
     'doors': '4 Doors',
     'transmission': 'Automatic',
-    'fuel': 'Gasoline'
+    'fuel': 'Gasoline',
+    'rate': '$30 / per day',
+    'img': 'honda-odyssey.png'
   },
 
-  'Mercedes G-Wagon': {
+  'Mercedes_GWagon': {
     'make': 'Mercedes',
     'model': 'G-Wagon',
     'doors': '4 Doors',
     'transmission': 'Automatic',
-    'fuel': 'Diesel'
+    'fuel': 'Diesel',
+    'rate': '$100 / per day',
   },
 
-  'Kia Sportage': {
+  'Kia_Sportage': {
     'make': 'Kia',
     'model': 'Sportage',
     'doors': '4 Doors',
     'transmission': 'Automatic',
-    'fuel': 'Gasoline'
+    'fuel': 'Gasoline',
+    'rate': '$40 / per day',
   },
 
-  'Lexus RX-350': {
+  'Lexus_RX350': {
     'make': 'Lexus',
     'model': 'RX-350',
     'doors': '4 Doors',
     'transmission': 'Automatic',
-    'fuel': 'Gasoline'
+    'fuel': 'Gasoline',
+    'rate': '$45 / per day',
   },
 
-  'Porsche 311 ': {
+  'Porsche_311': {
     'make': 'Porsche',
     'model': '311',
     'doors': '2 Doors',
     'transmission': 'Manual',
-    'fuel': 'Diesel'
+    'fuel': 'Diesel',
+    'rate': '$70 / per day',
   },
 
-  'Toyota Camry': {
+  'Toyota_Camry': {
     'make': 'Toyota',
     'model': 'Camry',
     'doors': '4 Doors',
     'transmission': 'Automatic',
-    'fuel': 'Gasoline'
+    'fuel': 'Gasoline',
+    'rate': '$40 / per day',
   }
 
 
 }
+  make.textContent = cars.Honda_Odyssey.make
+  model.textContent = cars.Honda_Odyssey.model
+  doors.textContent = cars.Honda_Odyssey.doors
+  transmission.textContent = cars.Honda_Odyssey.transmission
+  fuel.textContent = cars.Honda_Odyssey.fuel
+  rate.textContent = cars.Honda_Odyssey.rate
 
-console.log(cars)
+
+const hondaOdyssey = document.querySelector('#hondaOdyssey').addEventListener('click', () => {
+  make.textContent = cars.Honda_Odyssey.make
+  model.textContent = cars.Honda_Odyssey.model
+  doors.textContent = cars.Honda_Odyssey.doors
+  transmission.textContent = cars.Honda_Odyssey.transmission
+  fuel.textContent = cars.Honda_Odyssey.fuel
+  rate.textContent = cars.Honda_Odyssey.rate
+  image.src = cars.Honda_Odyssey.img
+})
+
+const mercedesGWagon = document.querySelector('#gWagon').addEventListener('click', () => {
+  make.textContent = cars.Mercedes_GWagon.make
+  model.textContent = cars.Mercedes_GWagon.model
+  doors.textContent = cars.Mercedes_GWagon.doors
+  transmission.textContent = cars.Mercedes_GWagon.transmission
+  fuel.textContent = cars.Mercedes_GWagon.fuel
+  rate.textContent = cars.Mercedes_GWagon.rate
+})
+
+const kiaSportage = document.querySelector('#kiaSportage').addEventListener('click', () => {
+  make.textContent = cars.Kia_Sportage.make
+  model.textContent = cars.Kia_Sportage.model
+  doors.textContent = cars.Kia_Sportage.doors
+  transmission.textContent = cars.Kia_Sportage.transmission
+  fuel.textContent = cars.Kia_Sportage.fuel
+  rate.textContent = cars.Kia_Sportage.rate
+})
+
+const lexusRX = document.querySelector('#lexusRX350').addEventListener('click', () => {
+  make.textContent = cars.Lexus_RX350.make
+  model.textContent = cars.Lexus_RX350.model
+  doors.textContent = cars.Lexus_RX350.doors
+  transmission.textContent = cars.Lexus_RX350.transmission
+  fuel.textContent = cars.Lexus_RX350.fuel
+  rate.textContent = cars.Lexus_RX350.rate
+})
+
+const porsche311 = document.querySelector('#porsche311').addEventListener('click', () => {
+  make.textContent = cars.Porsche_311.make
+  model.textContent = cars.Porsche_311.model
+  doors.textContent = cars.Porsche_311.doors
+  transmission.textContent = cars.Porsche_311.transmission
+  fuel.textContent = cars.Porsche_311.fuel
+  rate.textContent = cars.Porsche_311.rate
+})
+
+const toyotaCamry = document.querySelector('#toyotaCamry').addEventListener('click', () => {
+  make.textContent = cars.Toyota_Camry.make
+  model.textContent = cars.Toyota_Camry.model
+  doors.textContent = cars.Toyota_Camry.doors
+  transmission.textContent = cars.Toyota_Camry.transmission
+  fuel.textContent = cars.Toyota_Camry.fuel
+  rate.textContent = cars.Toyota_Camry.rate
+})
